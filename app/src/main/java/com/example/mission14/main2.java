26125841e0c165bbcf0127443669173011678c04
adir1 <aa7381@bs.amalnet.k12.il>
2024-05-06 15:50:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mission14/main2.java
@@ -0,0 +1,20 @@
+package com.example.mission14;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class main2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main2);
+
+    }
+}
